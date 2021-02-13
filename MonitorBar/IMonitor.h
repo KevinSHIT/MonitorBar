651d2ae4d973c8b941f8df18e83368a2f01e177b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-02-13 16:12:48 +0000

    fmt

--- MonitorBar/IMonitor.h
+++ MonitorBar/IMonitor.h
@@ -5,11 +5,11 @@
 class IMonitor
 {
 public:
-	virtual ~IMonitor() {}
-	virtual const std::basic_string<TCHAR> ToString()const = 0;
-	virtual const std::wstring ToLongString()const = 0;
-	virtual const double GetValue()const = 0;
-	virtual bool Init() = 0;
-	virtual void Update() = 0;
-	virtual void Reset() = 0;
+    virtual ~IMonitor() {}
+    virtual const std::basic_string<TCHAR> ToString()const = 0;
+    virtual const std::wstring ToLongString()const = 0;
+    virtual const double GetValue()const = 0;
+    virtual bool Init() = 0;
+    virtual void Update() = 0;
+    virtual void Reset() = 0;
 };
